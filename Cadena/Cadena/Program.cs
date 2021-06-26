2657d4f46a3fbddc8a331c4bda9c3b012b92d924
MiguelRamirez07 <86180811+MiguelRamirez07@users.noreply.github.com>
2021-06-26 00:35:35 -0600

    CADENA

---
+++ Cadena/Cadena/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Cadena
+{
+        class cadena
+        {
+            string Cadena;
+            public void nombre()
+            {
+                Console.WriteLine("introducir una palabra");
+                Cadena = Convert.ToString(Console.ReadLine());
+                char[] cArray = Cadena.ToCharArray();
+                string Reverse = String.Empty;
+                for (int i = cArray.Length - 1; i > -1; i--)
+                {
+                    Reverse += cArray[i];
+                }
+                Console.WriteLine(" alreves seria :" + Reverse);
+                Console.ReadKey();
+            }
+
+            public void Mayus()
+            {
+                Console.WriteLine("la cadena en  mayusculas seria: {0}", Cadena.ToUpper());
+                Console.ReadKey();
+                Console.WriteLine("la cadena cambiada seria: {0}", Cadena.Replace("a", "4").Replace("i", "1").Replace("s", "5").Replace("b", "8").Replace("e", "3").Replace("t", "7"));
+            }
+
+
+
+            static void Main(string[] args)
+            {
+                cadena objcade = new cadena();
+                objcade.nombre();
+                cadena objcadesa = new cadena();
+                objcade.Mayus();
+            }
+        }
+
+}
